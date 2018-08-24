057657b6fe2de4bd9cb7df33d4d2427f0f0c87e2
Preshit Harlikar <31642401+hpreshit@users.noreply.github.com>
2018-08-24 16:10:37 -0600

    Add files via upload

---
+++ Client-Server Dual Mode/GNU ARM v4.9.3 - Default/platform/emlib/src/em_usart.d
@@ -0,0 +1,151 @@
+platform/emlib/src/em_usart.o: ../platform/emlib/src/em_usart.c \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\emlib\inc/em_usart.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/em_device.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b232f256gm56.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\CMSIS\Include/core_cm4.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\CMSIS\Include/core_cmInstr.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\CMSIS\Include/cmsis_gcc.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\CMSIS\Include/core_cmFunc.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\CMSIS\Include/core_cmSimd.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/system_efr32bg1b.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_msc.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_emu.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_rmu.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_cmu.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_crypto.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_gpio_p.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_gpio.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_prs_ch.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_prs.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_ldma_ch.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_ldma.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_fpueh.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_gpcrc.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_timer_cc.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_timer.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_usart.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_leuart.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_letimer.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_cryotimer.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_pcnt.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_i2c.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_adc.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_acmp.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_idac.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_rtcc_cc.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_rtcc_ret.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_rtcc.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_wdog_pch.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_wdog.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_dma_descriptor.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_devinfo.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_romtable.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_prs_signals.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_dmareq.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_af_ports.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_af_pins.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\emlib\inc/em_cmu.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\emlib\inc/em_assert.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\emlib\inc/em_bus.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\emlib\inc/em_bus.h \
+ C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\emlib\inc/em_assert.h
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\emlib\inc/em_usart.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/em_device.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b232f256gm56.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\CMSIS\Include/core_cm4.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\CMSIS\Include/core_cmInstr.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\CMSIS\Include/cmsis_gcc.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\CMSIS\Include/core_cmFunc.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\CMSIS\Include/core_cmSimd.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/system_efr32bg1b.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_msc.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_emu.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_rmu.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_cmu.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_crypto.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_gpio_p.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_gpio.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_prs_ch.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_prs.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_ldma_ch.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_ldma.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_fpueh.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_gpcrc.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_timer_cc.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_timer.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_usart.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_leuart.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_letimer.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_cryotimer.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_pcnt.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_i2c.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_adc.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_acmp.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_idac.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_rtcc_cc.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_rtcc_ret.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_rtcc.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_wdog_pch.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_wdog.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_dma_descriptor.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_devinfo.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_romtable.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_prs_signals.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_dmareq.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_af_ports.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\Device\SiliconLabs\EFR32BG1B\Include/efr32bg1b_af_pins.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\emlib\inc/em_cmu.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\emlib\inc/em_assert.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\emlib\inc/em_bus.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\emlib\inc/em_bus.h:
+
+C:\Users\Preshit\SimplicityStudio\v4_workspace\Particle_Sensor_\platform\emlib\inc/em_assert.h:
